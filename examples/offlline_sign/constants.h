4224ed2b2804e8b298ef039654d02acd4620c5e6
Gustbel <gustavobelbruno@gmail.com>
2025-06-07 16:23:41 -0300

    add offline_sign example and setup global build system

--- examples/offlline_sign/constants.h
+++ examples/offlline_sign/constants.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+const char* message_hex;
 extern const uint8_t private_key[32];
 
 #endif
---
+++ examples/offlline_sign/offline_sign.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "microsui/sign.h"
+#include "constants.h"
+
+
+int main() {
+    printf("\n\n\t\t\t --- SUI OFFLINE SIGN DEMO ---\n");
+    printf("\n\tOriginal Message: %s", message_hex);
+
+    uint8_t sui_sig[97];
+    microsui_sign_offline(sui_sig, message_hex, private_key);
+    printf("\n\n\n  Generating Signature...\n\n");
+    print_hex("\t Sui Signature", sui_sig, 97);
+    printf("\n\t SIGNATURE must be sended to the Gateway to be broadcasted to the Sui Network...\n");
+
+    return 0;
+}
