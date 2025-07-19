918c5647940304ff41d6c4db71360f03f8d03a92
Gustbel <gustavobelbruno@gmail.com>
2025-07-19 14:35:39 +0300

    added arduino files and examples

---
+++ src/MicroSui.h
@@ -0,0 +1,17 @@
+#ifndef MICROSUI_H
+#define MICROSUI_H
+/* MicroSui.h is the main header file for the Arduino MicroSui library */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "sign.h"
+#include "utils.h"
+#include "cryptography.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
