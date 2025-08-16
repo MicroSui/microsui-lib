0ca85b20b318f38c295b4abaf408394a7502d0f6
Gustbel <gustavobelbruno@gmail.com>
2025-08-16 22:14:40 +0300

    trying attempt one

--- src/http_router.h
+++ src/http_router.h
@@ -3,19 +3,11 @@
 
 #include <stddef.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 int microsui_http_post(const char* host,
               const char* path,
               int port,
               const char* jsonBody,
               char* responseOut,
               size_t maxLen);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
