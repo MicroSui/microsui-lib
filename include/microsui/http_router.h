fc7ca2ad644130e29a936d393897f94f98a93467
Gustbel <gustavobelbruno@gmail.com>
2025-08-17 01:15:36 +0300

    update one

--- include/microsui/http_router.h
+++ include/microsui/http_router.h
@@ -3,12 +3,7 @@
 
 #include <stddef.h>
 
-int microsui_http_post(const char* host,
-              const char* path,
-              int port,
-              const char* jsonBody,
-              char* responseOut,
-              size_t maxLen);
+int microsui_http_post(const char* host, const char* path, int port, const char* jsonBody, char* responseOut, size_t maxLen);
 
 int microsui_connect_wifi(const char* ssid, const char* password);
 
