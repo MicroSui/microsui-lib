b219c5c5b027246568f5bd8fb70fb6cd6e630676
Gustbel <gustavobelbruno@gmail.com>
2025-08-17 14:52:58 +0300

    trying new msg saver

--- src/impl/http/http_esp32_arduino.cpp
+++ src/impl/http/http_esp32_arduino.cpp
@@ -2,6 +2,7 @@
 
 extern "C" {
 #include "http_router.h"
+#include "microsui/rpc_json_builder.h"
 }
 #include <Arduino.h>
 #include <HTTPClient.h>
@@ -12,79 +13,91 @@ extern "C" {
 WiFiMulti WiFiMulti;
 
 void setClock() {
-  configTime(0, 0, "pool.ntp.org");
+    configTime(0, 0, "pool.ntp.org");
 
-  //Serial.print(F("Waiting for NTP time sync: "));
-  time_t nowSecs = time(nullptr);
-  while (nowSecs < 8 * 3600 * 2) {
-    delay(500);
-    Serial.print(F("."));
-    yield();
-    nowSecs = time(nullptr);
-  }
+    //Serial.print(F("Waiting for NTP time sync: "));
+    time_t nowSecs = time(nullptr);
+    while (nowSecs < 8 * 3600 * 2) {
+        delay(500);
+        Serial.print(F("."));
+        yield();
+        nowSecs = time(nullptr);
+    }
 
-  Serial.println();
-  struct tm timeinfo;
-  gmtime_r(&nowSecs, &timeinfo);
-  Serial.print(F("Current time: "));
-  Serial.print(asctime(&timeinfo));
+    Serial.println();
+    struct tm timeinfo;
+    gmtime_r(&nowSecs, &timeinfo);
+    Serial.print(F("Current time: "));
+    Serial.print(asctime(&timeinfo));
 }
 
 int microsui_connect_wifi(const char* ssid, const char* password) {
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP(ssid, password);
+    WiFi.mode(WIFI_STA);
+    WiFiMulti.addAP(ssid, password);
 
-  Serial.print("Waiting for WiFi to connect...");
-  while ((WiFiMulti.run() != WL_CONNECTED)) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println(" connected");
-  
-  return 0;
+    Serial.print("Waiting for WiFi to connect...");
+    while ((WiFiMulti.run() != WL_CONNECTED)) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println(" connected");
+    
+    return 0;
 }
 
 
 int microsui_http_post(const char* host, const char* path, int port, const char* jsonBody, char* responseOut, size_t maxLen) {
-  //setClock();
+    //setClock(); // Uncomment this line if you want to set the clock using NTP
 
-  NetworkClientSecure *client = new NetworkClientSecure;
-  if (client) {
-    client->setInsecure();
+    NetworkClientSecure *client = new NetworkClientSecure;
+    if (client) {
+        // client->setCACert(rootCACertificate); // Uncomment this line if you have a specific CA secure certificate to use
+        client->setInsecure();
 
-    // client->setCACert(rootCACertificate);
+        HTTPClient https;
+        Serial.print("[HTTPS] begin...\n");
 
-    HTTPClient https;
-    Serial.print("[HTTPS] begin...\n");
+        String url = "https://" + String(host) + ":" + String(port) + String(path);
+        if (https.begin(*client, url)) {
+            https.addHeader("Content-Type", "application/json");
 
-    String url = "https://" + String(host) + ":" + String(port) + String(path);
-    if (https.begin(*client, url)) {
-      https.addHeader("Content-Type", "application/json");
+            Serial.print("[HTTPS] POST...\n");
+            int httpCode = https.POST(jsonBody);
 
-      Serial.print("[HTTPS] POST...\n");
-      int httpCode = https.POST(jsonBody);
+            if (httpCode > 0) {
+                Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
+                if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_CREATED) {
+                    String payload = https.getString();
+                    //Serial.println(payload);
 
-      if (httpCode > 0) {
-        Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
+                    // Prepare the JSON-RPC request string - Using a StringBuilder for efficient string concatenation
+                    StringBuilder sb;
+                    if (!sb_init(&sb, 1600, 10000)) {
+                        return -1;
+                    }
 
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_CREATED) {
-          String payload = https.getString();
-          Serial.println(payload);
+                    // Append the response to the StringBuilder
+                    sb_append(&sb, payload.c_str());
+
+                    responseOut = sb_detach(&sb);
+
+                    https.end();
+                    delete client;
+
+                    return 0;
+                }
+            } else {
+                Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
+            }
+            https.end();
+        } else {
+            Serial.printf("[HTTPS] Unable to connect to %s\n", url.c_str());
         }
-      } else {
-        Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpCode).c_str());
-      }
-
-      https.end();
+        delete client;
     } else {
-      Serial.printf("[HTTPS] Unable to connect\n");
+        Serial.println("Unable to create client");
     }
-
-    delete client;
-  } else {
-    Serial.println("Unable to create client");
-  }
-  return 0;
+    return -1; // Default error case
 }
 
 #endif
