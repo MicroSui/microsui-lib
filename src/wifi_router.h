3546e2e79177f6b0718781d2e4b8373dea3784b3
Gustbel <gustavobelbruno@gmail.com>
2025-08-18 14:53:17 +0300

    forgot to modif these headers too

--- src/wifi_router.h
+++ src/wifi_router.h
@@ -7,10 +7,6 @@
     (defined(ARDUINO) && defined(ARDUINO_ARCH_ESP32)) \
 )
 
-#if !WIFI_SUPPORTED_PLATFORMS
-#   error "MicroSui: WiFi support isn’t available on this platform/board yet. To use it here, add an adapter for your platform (implement microsui_http_post) and save it in src/impl/http/http_<your_platform>.extension"
-#endif
-
 int microsui_connect_wifi(const char* ssid, const char* password);
 
 #endif
