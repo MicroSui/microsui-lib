d8e2d5cc56d6f0464aba26348fbd07e04fd53801
Gustbel <gustavobelbruno@gmail.com>
2025-08-18 13:56:25 +0300

    fixed error no supported message in http adapters

--- src/http_router.h
+++ src/http_router.h
@@ -4,10 +4,12 @@
 #include "utils/string_builder.h" // For StringBuilder usage, util for string managment
 #include <stddef.h>
 
-#define SUPPORTED_PLATFORMS  (defined(ARDUINO) && defined(ARDUINO_ARCH_ESP32_)) || \
-    defined(_WIN32) || defined(__linux__) || defined(__APPLE__) || defined(__unix__) || // Desktop platforms
+#define HTTP_SUPPORTED_PLATFORMS ( \
+    (defined(ARDUINO) && defined(ARDUINO_ARCH_ESP32)) || \
+    defined(_WIN32) || defined(__linux__) || defined(__APPLE__) || defined(__unix__) \
+)
 
-#if MICROSUI_SUPPORTED_PLATFORMS
+#if !HTTP_SUPPORTED_PLATFORMS
 #   error "MicroSui: HTTP support isn’t available on this platform/board yet. To use it here, add an adapter for your platform (implement microsui_http_post) and save it in src/impl/http/http_<your_platform>.extension"
 #endif
 
