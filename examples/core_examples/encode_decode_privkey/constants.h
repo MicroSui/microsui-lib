e8fe45bb668700c05a317713969c2c0f8cf9506d
Gustbel <gustavobelbruno@gmail.com>
2025-08-27 06:24:02 +0300

    deleted constant files in core examples

--- examples/core_examples/encode_decode_privkey/constants.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-#include <stdint.h>
-
-const char *private_key_1_bech32;
-const uint8_t private_key_2[32];
-
-#endif
--- examples/core_examples/offline_sign/constants.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-#include <stdint.h>
-
-const char* message_hex;
-extern const uint8_t private_key[32];
-
-#endif
