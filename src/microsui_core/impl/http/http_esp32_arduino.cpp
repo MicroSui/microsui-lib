542ab0b3ada96a36eda3ca47843dad2a60aecc58
Gustbel <gustavobelbruno@gmail.com>
2025-08-27 23:57:58 +0300

    fixed print logs in Arduino files

--- src/microsui_core/impl/http/http_esp32_arduino.cpp
+++ src/microsui_core/impl/http/http_esp32_arduino.cpp
@@ -73,11 +73,11 @@ char* microsui_http_post(const char* host, const char* path, int port, const cha
                     return buf;
                 }
             } else {
-                Serial.println("[MicroSui HTTP Client]: Transaction send operation failed, error: %s", https.errorToString(httpCode).c_str());
+                Serial.printf("[MicroSui HTTP Client]: Transaction send operation failed, error: %s\n", https.errorToString(httpCode).c_str());
             }
             https.end();
         } else {
-            Serial.println("[MicroSui HTTP Client]: Unable to connect to RPC: '%s'", url.c_str());
+            Serial.printf("[MicroSui HTTP Client]: Unable to connect to RPC: '%s'\n", url.c_str());
         }
         delete client;
     } else {
