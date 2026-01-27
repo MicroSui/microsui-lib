71d9ec160a7b8eeab67ed37fcea380b27e1025e7
Gustbel <gustavobelbruno@gmail.com>
2026-01-27 15:06:05 +0200

    fixed some console print in new arduino example

--- examples/ArduinoExamples/ValidatingSignatures/ValidatingSignatures.ino
+++ examples/ArduinoExamples/ValidatingSignatures/ValidatingSignatures.ino
@@ -18,7 +18,7 @@ void setup()
     Serial.println(F("=== MicroSui ESP32 — Validation of a Sui Signature ==="));
 
     // 1) Generating Keypair, and Transactions Objects
-    Serial.println(F("\n [1/5] Generating Keypair, and Transactions Objects..."));
+    Serial.println(F("\n [1/4] Generating Keypair, and Transactions Objects..."));
     // Create a keypair from a given secret key in Bech32 format
     MicroSuiEd25519 keypair = SuiKeypair_fromSecretKey(sui_private_key_bech32);
     // Create two transactions with the prebuilted TxBytes
@@ -26,7 +26,7 @@ void setup()
     MicroSuiTransaction tx2 = SuiTransaction_setPrebuiltTxBytes(message2_string);
 
     // 2) Generating Keypair, and Transactions Objects
-    Serial.println(F("\n [2/5] Signing the Transaction Message1 with the keypair..."));
+    Serial.println(F("\n [2/4] Signing the Transaction Message1 with the keypair..."));
     SuiSignature sig = keypair.signTransaction(&keypair, message1_string);
     
     Serial.println(F("\n --- Signature Result ---"));
@@ -39,13 +39,13 @@ void setup()
     }
 
     // 3) Validating Message1 with the signature (Must be Correct)
-    Serial.println(F("\n [3/5] Validating Message1 with the signature (Must be Correct)..."));
+    Serial.println(F("\n [3/4] Validating Message1 with the signature (Must be Correct)..."));
     int verification_result_1 = microsui_verify_signature(sig.bytes, tx1.tx_bytes.data, tx1.tx_bytes.length);
     if (verification_result_1 == 0)
         Serial.println(F("\t   - Signature Verification is CORRECT for Message 1 -- OK\n"));
 
     // 4) Validating Message2 with the signature (Must be FAILED)
-    Serial.println(F("\n [3/5] Validating Message2 with the signature (Must be FAILED)..."));
+    Serial.println(F("\n [4/4] Validating Message2 with the signature (Must be FAILED)..."));
     int verification_result_2 = microsui_verify_signature(sig.bytes, tx2.tx_bytes.data, tx2.tx_bytes.length);
     if (verification_result_2 == 0)
         Serial.println(F("\t   - Signature Verification is FAILED for Message 2 -- OK\n\n"));
